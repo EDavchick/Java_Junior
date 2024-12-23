de49fa6011a6c270bf0e1db0905976c7cf76c1a3
ED <154407386+EDavchick@users.noreply.github.com>
2024-12-23 17:46:23 +0300

    add lesson 02

---
+++ L02/less02/task3/Entity.java
@@ -0,0 +1,12 @@
+package org.dav.less02.task3;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Entity {
+
+}
